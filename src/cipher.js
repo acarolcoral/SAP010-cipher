825481ba699235e6a10dd3a2aced061c16e9c74f
carolina <carolina.menezes.dev@gmail.com>
2023-05-23 15:29:48 -0300

    Versão que funciona 1

--- src/cipher.js
+++ src/cipher.js
@@ -12,9 +12,10 @@ const cipher = {
           const caractereCodificado = palavra.charCodeAt(i); // Transformar em códico ASCIi
           if (caractereCodificado >= 65 && caractereCodificado <= 91) { // Verifica se está entre o código de A a Z em ASCII.
             caractere = String.fromCharCode(((caractereCodificado - 65 + deslocamento) % 26) + 65); // Cifrar e transformar em String
-            palavraCodificada = caractere; // Iguala a variável que guarda o resiltado.
+            palavraCodificada += caractere; // concatenação
+            // palavraCodificada = caractere; // Iguala a variável que guarda o resiltado.
           }
-        }        
+        }  
       }
       return palavraCodificada; // Retorna o resultado
     }
@@ -33,9 +34,10 @@ const cipher = {
           const caractereCodificado = palavra.charCodeAt(i); // Transformar em códico ASCIi
           if (caractereCodificado >= 65 && caractereCodificado <= 91) { // Verifica se está entre o código de A a Z em ASCII.
             caractere = String.fromCharCode(((caractereCodificado - 65 - deslocamento) % 26) + 65); // Decifrar e transformar em String
-            palavraDecodificada = caractere; // Iguala a variável que guarda o resiltado.
+            palavraDecodificada += caractere;
+            //palavraDecodificada = caractere; // Iguala a variável que guarda o resiltado.
           }
-        }        
+        }   
       }
       return palavraDecodificada; // Retorna o resultado
     }
